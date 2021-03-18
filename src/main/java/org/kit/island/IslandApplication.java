ac02bea20dde5978054b64eae21c73dff77af083
p.shcharbakov <Pavel.Shcharbakov@artezio.com>
2021-03-18 12:19:15 +0300

    init back

---
+++ src/main/java/org/kit/island/IslandApplication.java
@@ -0,0 +1,13 @@
+package org.kit.island;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IslandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IslandApplication.class, args);
+	}
+
+}
---
+++ src/main/resources/templates/public_html/js/form.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+  
+  $('input[type="tel"]').inputmask('+7 (999) 999 99 99', {
+    clearMaskOnLostFocus: true,
+  });
+  
+  $('.contact_form').submit(function () {
+    var form = $(this);
+    var field = [];
+    form.find('input[data-validate]').each(function () {
+      field.push('input[data-validate]');
+      var value = $(this).val(),
+          line = $(this).closest('.some-form__line');
+      for(var i=0;i<field.length;i++) {
+        if( !value ) {
+          line.addClass('some-form__line-required');
+          setTimeout(function() {
+            line.removeClass('some-form__line-required')
+          }.bind(this),2000);
+          event.preventDefault();
+        }
+      }
+    });
+  });
+});
