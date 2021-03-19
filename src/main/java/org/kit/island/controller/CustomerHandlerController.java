f3dfed71e85e071486a791ab41bbe2b04732406a
p.shcharbakov <Pavel.Shcharbakov@artezio.com>
2021-03-19 19:21:19 +0300

    Enable async handling appeal

--- src/main/java/org/kit/island/controller/CustomerHandlerController.java
+++ src/main/java/org/kit/island/controller/CustomerHandlerController.java
@@ -3,25 +3,33 @@ package org.kit.island.controller;
 import lombok.RequiredArgsConstructor;
 import org.kit.island.model.CustomerMessage;
 import org.kit.island.service.CustomerHandlerService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 public class CustomerHandlerController {
 
     private final CustomerHandlerService customerHandlerService;
 
     @PostMapping("/customer")
-    public void handleCustomerMessage(@RequestParam Map<String, String> body) {
+    public String handleCustomerMessage(@RequestParam Map<String, String> body) {
         CustomerMessage customerMessage = new CustomerMessage();
         customerMessage.setName(body.get("myname"));
         customerMessage.setPhone(body.get("phone"));
         customerMessage.setMessage(body.get("mytext"));
         customerHandlerService.handleCustomerMessage(customerMessage);
+        return "redirect:index";
+    }
+
+    @GetMapping("/index")
+//    @ResponseBody
+    public String welcome() {
+        return "/public_html/index.html";
     }
 
 }
--- src/main/java/org/kit/island/service/CustomerHandlerService.java
+++ src/main/java/org/kit/island/service/CustomerHandlerService.java
@@ -5,6 +5,7 @@ import org.kit.island.model.CustomerMessage;
 import org.kit.island.repository.CustomerMessageRepository;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,6 +18,7 @@ public class CustomerHandlerService {
     private final JavaMailSender emailSender;
     private final CustomerMessageRepository customerMessageRepository;
 
+    @Async
     public void handleCustomerMessage(CustomerMessage customerMessage) {
         customerMessageRepository.save(customerMessage);
         SimpleMailMessage message = new SimpleMailMessage();
