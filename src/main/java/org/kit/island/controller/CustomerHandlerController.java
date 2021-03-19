69c2e4ea0ccd381e730c537b4a0def280849f607
p.shcharbakov <Pavel.Shcharbakov@artezio.com>
2021-03-19 20:26:25 +0300

    add index url

--- src/main/java/org/kit/island/controller/CustomerHandlerController.java
+++ src/main/java/org/kit/island/controller/CustomerHandlerController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Map;
 
@@ -17,19 +18,19 @@ public class CustomerHandlerController {
     private final CustomerHandlerService customerHandlerService;
 
     @PostMapping("/customer")
+    @ResponseBody
     public void handleCustomerMessage(@RequestParam Map<String, String> body) {
         CustomerMessage customerMessage = new CustomerMessage();
         customerMessage.setName(body.get("myname"));
         customerMessage.setPhone(body.get("phone"));
         customerMessage.setMessage(body.get("mytext"));
         customerHandlerService.handleCustomerMessage(customerMessage);
-//        return "forward:/static/index.html";
+//        return "forward:/index.html";
     }
 
     @GetMapping("/index")
-//    @ResponseBody
     public String welcome() {
-        return "/public_html/index.html";
+        return "/index.html";
     }
 
 }
